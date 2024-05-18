b85d3582da9e077cb62bb89a63c4593fef5ce1f8
Felipe Borges <felipetecacc@gmail.com>
2024-05-18 12:54:04 -0300

    feat: add folders structure

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { server } from './server/Server'; 
 
-server.listen(3333, () => {
-  console.log('APP Rodando!');
+server.listen(process.env.PORT || 3333, () => {
+  console.log(`App rodando na porta ${process.env.PORT}`);
 });
