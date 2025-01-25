d28f49d01f2422180f562c6382af22b6d3133f4d
Felipe Borges <felipetecacc@gmail.com>
2025-01-25 13:57:48 -0300

    feat: added migration to cidade table

---
+++ src/server/database/migrations/0000_create_cidade.ts
@@ -0,0 +1,21 @@
+import { Knex } from 'knex';
+import { ETableNames } from '../ETableNames';
+
+export async function up(knex: Knex) {
+  return knex.schema
+    .createTable('cidade', (table) => {
+      table.bigIncrements('id').primary().index();
+      table.string('nome', 150).index().notNullable();
+
+      table.comment('Tabela usadaa para armazenar cidades do sistema');
+    })
+    .then(() => {
+      console.log(`# Created table ${ETableNames.cidade}`);
+    });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('cidade').then(() => {
+    console.log(`# Dropped table ${ETableNames.cidade}`);
+  });
+}
