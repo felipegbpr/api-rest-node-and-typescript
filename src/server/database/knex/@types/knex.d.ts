ad5220c4e578141b639112d61ab75a9a986bf669
Felipe Borges <felipetecacc@gmail.com>
2025-02-01 17:12:57 -0300

    feat: added migrations to usuario table

--- src/server/database/knex/@types/knex.d.ts
+++ src/server/database/knex/@types/knex.d.ts
@@ -1,10 +1,10 @@
-import { ICidade } from '../../models';
+import { ICidade, IPessoa, IUsuario } from '../../models';
 
 /* Declares table typings within knex */
 declare module 'knex/types/tables' {
   interface Tables {
     cidade: ICidade;
     pessoa: IPessoa;
-    // usuario: IUsuario
+    usuario: IUsuario;
   }
 }
