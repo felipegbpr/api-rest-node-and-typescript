16a0d04e602cef22851532ba11b0010bd243a4d2
Felipe Borges <felipetecacc@gmail.com>
2025-02-01 13:56:02 -0300

    feat: added providers to pessoas table

--- src/server/database/knex/@types/knex.d.ts
+++ src/server/database/knex/@types/knex.d.ts
@@ -1,9 +1,10 @@
 import { ICidade } from '../../models';
 
+/* Declares table typings within knex */
 declare module 'knex/types/tables' {
   interface Tables {
     cidade: ICidade;
-    // pessoa: IPessoa
+    pessoa: IPessoa;
     // usuario: IUsuario
   }
 }
