72439b36f656587db4fb2f88d685bebd74f086b5
Felipe Borges <felipetecacc@gmail.com>
2025-02-08 13:44:41 -0300

    feat: added authentication to cidades e pessoas endpoints

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -5,6 +5,7 @@ import {
   PessoasController,
   UsuariosController,
 } from './../controllers';
+import { ensureAuthenticated } from '../shared/middleware';
 
 const router = Router();
 
@@ -15,53 +16,72 @@ router.get('/', (_, res) => {
 router.get(
   '/cidades',
   CidadesController.getAllValidation,
+  ensureAuthenticated,
   CidadesController.getAll
 );
 router.get(
   '/cidades/:id',
+  ensureAuthenticated,
   CidadesController.getByIdValidation,
+  ensureAuthenticated,
   CidadesController.getById
 );
 router.post(
   '/cidades',
+  ensureAuthenticated,
   CidadesController.createValidation,
+  ensureAuthenticated,
   CidadesController.create
 );
 router.put(
   '/cidades/:id',
+  ensureAuthenticated,
   CidadesController.updateByIdValidation,
+  ensureAuthenticated,
   CidadesController.updateById
 );
 router.delete(
   '/cidades/:id',
+  ensureAuthenticated,
   CidadesController.deleteByIdValidation,
+  ensureAuthenticated,
   CidadesController.deleteById
 );
 
 // * Pessoas Routes * //
 router.get(
   '/pessoas',
+  ensureAuthenticated,
   PessoasController.getAllValidation,
+  ensureAuthenticated,
   PessoasController.getAll
 );
 router.post(
   '/pessoas',
+  ensureAuthenticated,
   PessoasController.createValidation,
+  ensureAuthenticated,
   PessoasController.create
 );
 router.get(
   '/pessoas/:id',
+  ensureAuthenticated,
   PessoasController.getByIdValidation,
+  ensureAuthenticated,
   PessoasController.getById
 );
 router.put(
   '/pessoas/:id',
+  ensureAuthenticated,
   PessoasController.updateByIdValidation,
+  ensureAuthenticated,
   PessoasController.updateById
 );
 router.delete(
   '/pessoas/:id',
+  ensureAuthenticated,
   PessoasController.deleteByIdValidation,
+  ensureAuthenticated,
   PessoasController.deleteById
 );
 
